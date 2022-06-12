f7305eb28b1df0c0df4dc5248ac3a12c5c08a31e
Bianca Emmanuel <bianca.emmanuel@student.moringaschool.com>
2022-06-12 08:22:05 +0100

    commit tree 6997cdc81d930cecc75e055c28c75817a5b5757c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(dActivity = "This Saturday, I want to roller-skate!") {
+    console.log(dActivity);
+    if (dActivity === "This Saturday, I want to roller-skate!"){
+        return dActivity
+      } else {
+        return "This Saturday, I want to bathe my dog!"
+      }     
+}
+
+function mondayWork(nActivity = "This Monday, I will go to the office.") {
+    console.log(nActivity);
+    if (nActivity === "This Monday, I will go to the office."){
+        return nActivity
+    } else {
+        return "This Monday, I will work from home."
+    }
+}
+
+function wrapAdjective(result = '*', emphatic = "a hard worker") {
+    console.log();
+    if (result === '*' && emphatic === "a hard worker"){
+        return `You are ${result} ${emphatic} ${result}!`
+    }else if (result === '||'){
+        return `You are ${result} ${emphatic} ${result}!`
+    }
+}
+
+
+   
